807ff3248afe72b864410dc972b1897f54bf9b5c
David Yusaku <davidyusaku13@gmail.com>
2024-03-07 09:23:01 +0700

    add feature : py yaml
    successfully read and write a yaml file

--- tes.py
+++ tes.py
@@ -1,13 +1,13 @@
 import yaml
 
-with open('file.yml', 'r') as file:
+with open('input.yml', 'r') as file:
     data = yaml.safe_load(file)
 
 # for i in range(len(data)):
 #     print(f"Item: {data[i]['item']} | Value: {
 #           data[i]['value']} | Desc: {data[i]['desc']}")
 
-with open('names.yml', 'w') as file:
+with open('output.yml', 'w') as file:
     yaml.dump(data, file)
 
-print(open('names.yml').read())
+print(open('output.yml').read())
